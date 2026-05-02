44bed36fb7071b2933d8f33fbc841531071c66fe
Eddie Elam <edward.elam@gmail.com>
2026-05-02 19:44:19 +0000

    VITE BACK TO OG

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,18 +1,3 @@
 /// <reference types="vite/client" />
 
-interface Window {
-  webkit?: {
-    messageHandlers: {
-      initiateNfcHandshake: {
-        postMessage: (message: any) => void;
-      };
-      syncHealthData: {
-        postMessage: (message: any) => void;
-      };
-    };
-  };
-  // IDIA Native Callbacks
-  onNfcHandshakeComplete?: (token: string) => void;
-  onNfcHandshakeError?: (error: string) => void;
-  onHealthSyncComplete?: (payload: any) => void;
-}
+i
