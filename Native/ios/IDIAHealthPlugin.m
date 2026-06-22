f86e504e0474bc1f3ff2a45231948942ab93a4fa
Eddie Elam <edward.elam@gmail.com>
2026-06-22 13:17:37 +0000

    Removing the iOS stuff in the wrong place

--- Native/ios/IDIAHealthPlugin.m
+++
@@ -1,8 +0,0 @@
-#import <Capacitor/Capacitor.h>
-
-CAP_PLUGIN(IDIAHealthPlugin, "IDIAHealth",
-    CAP_PLUGIN_METHOD(checkAvailability, CAPPluginReturnPromise);
-    CAP_PLUGIN_METHOD(requestPermissions, CAPPluginReturnPromise);
-    CAP_PLUGIN_METHOD(checkPermissions, CAPPluginReturnPromise);
-    CAP_PLUGIN_METHOD(getHealthData, CAPPluginReturnPromise);
-)
